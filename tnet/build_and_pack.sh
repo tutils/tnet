04727a03298797931331781b52591ccb309d04f4
t5w0rd <lightning_0721@163.com>
2022-05-24 06:34:27 +0800

    build arm64

--- tnet/build_and_pack.sh
+++ tnet/build_and_pack.sh
@@ -21,3 +21,20 @@ export GOOS="windows"
 go build && \
     (zip -r -q -o $TARGET-$GOOS-$GOARCH.zip $TARGET.exe; \
     rm $TARGET.exe)
+
+export GOARCH="arm64"
+
+export GOOS="linux"
+go build && \
+    (zip -r -q -o $TARGET-$GOOS-$GOARCH.zip $TARGET; \
+    rm $TARGET)
+
+export GOOS="darwin"
+go build && \
+    (zip -r -q -o $TARGET-$GOOS-$GOARCH.zip $TARGET; \
+    rm $TARGET)
+
+export GOOS="windows"
+go build && \
+    (zip -r -q -o $TARGET-$GOOS-$GOARCH.zip $TARGET.exe; \
+    rm $TARGET.exe)
