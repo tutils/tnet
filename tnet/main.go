e12c3eac4536afd6716d98f5fd081444713f2446
t5w0rd <lightning_0721@163.com>
2025-06-27 11:21:09 +0800

    mockCmdline

--- tnet/main.go
+++ tnet/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
+	"strings"
 
 	// _ "net/http/pprof"
 
 	"github.com/tutils/tnet/cmd"
 )
 
+func mockCmdline(cmdline string) {
+	os.Args = append(os.Args[:1], strings.Split(cmdline, " ")...)
+}
+
 func main() {
 	// go http.ListenAndServe(":", nil)
 	log.SetFlags(log.Ltime | log.Lshortfile)
+
+	// mockCmdline(`httpsrv -l :28080`)
+
 	cmd.Execute()
 }
